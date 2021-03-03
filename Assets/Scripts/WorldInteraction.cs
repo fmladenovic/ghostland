b47f179625a594ca53ecd8a376d1e8bf7455faca
Filip <filipeng97@gmail.com>
2021-03-03 19:11:55 +0100

    Init

---
+++ Assets/Scripts/WorldInteraction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class WorldInteraction : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+        {
+            GetInteraction();
+        }
+    }
+
+    void GetInteraction()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if (Physics.Raycast(interactionRay. out interactionInfo, Mathf.infinity))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if (interactedObject.tag == "Interactable Ojbect")
+            {
+                Debug.Log("Interactable interacted.");
+            } else
+            {
+
+            }
+        }
+    }
+}
